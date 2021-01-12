6b81dd4c7db4a5bd50122ffbf7d31c40c6c32687
sunnyraj5555 <headheal2206@gmail.com>
2021-01-12 14:02:39 +0530

    Updated Enter three Number

--- Sum.cpp
+++ Sum.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main()
 {
-	int a,b;
+	int a,b,c;
 	cout <<"Enter two Numbers:"<<endl;
 	cin >> a >> b;
-	cout << "Sum =" << a+b <<endl;
-
+	cout << "Sum of Two numbers =" << a+b <<endl;
+	cout << "Enter three Number"<< endl;
+	cin >> c;
+	cout << "Sum of three numbers =" << a+b+c <<endl;
 return 0;
 
 }
