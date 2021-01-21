1004bbe1507f6c0dfa3deaa671690efca057d303
sunnyraj5555 <headheal2206@gmail.com>
2021-01-21 14:07:53 +0530

    Added the condition for Checking LeapYear

--- LeapYear.cpp
+++ LeapYear.cpp
@@ -9,8 +9,19 @@ int main()
 
 bool LeapYear(int year)
 {
-
-
-
+	bool isLeapYear =false;
+	
+	if (year%4 == 0)
+	{
+		if(year%100 ==0)
+		{
+			if(year%400 == 0)
+			{
+				isLeapYear = true;
+			}
+		}
+		else isLeapYear = true;
+	}
+	return isLeapYear;
 
 }
