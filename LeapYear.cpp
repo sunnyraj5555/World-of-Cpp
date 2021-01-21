50971b844701c11382927e01ec5c111bb84e5116
sunnyraj5555 <headheal2206@gmail.com>
2021-01-21 14:11:03 +0530

    Done documentation of LeapYear() function

--- LeapYear.cpp
+++ LeapYear.cpp
@@ -11,13 +11,13 @@ bool LeapYear(int year)
 {
 	bool isLeapYear =false;
 	
-	if (year%4 == 0)
+	if (year%4 == 0)  //for Year divisible by 4 is leap year
 	{
-		if(year%100 ==0)
+		if(year%100 ==0)  // If century year then may or may not be leap year
 		{
-			if(year%400 == 0)
+			if(year%400 == 0) // If century year is divisible by 400 it is leap year.
 			{
-				isLeapYear = true;
+				isLeapYear = true;  //true indicates its leap year
 			}
 		}
 		else isLeapYear = true;
