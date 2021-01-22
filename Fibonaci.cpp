828f63262f0022449870db66519e9def72a4f94c
sunnyraj5555 <headheal2206@gmail.com>
2021-01-22 16:46:50 +0530

    Commit 2

--- Fibonaci.cpp
+++ Fibonaci.cpp
@@ -5,4 +5,30 @@ using namespace std;
 
 int Fib(int);
 
+int main()
+{
+	int n;
+	cout<<"Enter the number till which you need the Fibonaci Series: ";
+	cin >> n;
+	for(int i=1;i<=n;i++)
+	{
+	cout<< Fib(i)<< " ";	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+                                [ Read 8 lines ]
+^G Get Help  ^O Write Out ^W Where Is  ^K Cut Text  ^J Justify   ^C Cur Pos
+^X Exit      ^R Read File ^\ Replace   ^U Paste Text^T To Spell  ^_ Go To Line
 
