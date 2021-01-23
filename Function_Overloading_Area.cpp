fa7fa89c07337517d451f5213aca821f73e1b079
sunnyraj5555 <headheal2206@gmail.com>
2021-01-24 00:49:40 +0530

    Area of Circle and Rectangle

--- Function_Overloading_Area.cpp
+++ Function_Overloading_Area.cpp
@@ -4,4 +4,26 @@ using namespace std;
 float area(int); //for area of circle
 int area(int,int); //for area of rectangle
 
+int main()
+{
+	int r;
+	cout << "Enter radius of Circle: ";
+	cin >> r;
+	cout <<"Area of Circle is ="<< area(r)<< endl;
 
+	int l,b;
+	cout << "Enter the Length and breadth of Rectangle: ";
+	cin >> l >> b;
+	cout <<"Area of Rectangle =" <<area(l,b)<<endl;
+	return 0;
+}
+
+float area(int r)
+{
+	return (3.14*r*r);
+}
+
+int area(int l, int b)
+{
+	return (l*b);
+}
