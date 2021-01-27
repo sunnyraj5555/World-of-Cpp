dff21f3b93484b4e26bd9a6fea67dbfbaa44ead2
sunnyraj5555 <headheal2206@gmail.com>
2021-01-27 20:44:22 +0530

    Function Declaration and Defination Done

--- Struct_C_type.cpp
+++ Struct_C_type.cpp
@@ -9,3 +9,19 @@ struct book
 
 }:
 
+void display(book);
+book input();
+
+          //Creating a function for output
+void display(book b)
+{
+	cout <<b.bookid <<" "<< b.title <<" "<<b.price <<endl;
+}
+          //creating a function of book return type 
+book input()
+{
+	book b;
+	cout << "Enter bookid, title and price of book:" <<endl;
+	cin >>b.bookid >>b.title >>b.price ;
+	return(b);
+}
