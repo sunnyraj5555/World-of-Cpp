0ef9aa719178c650250884a01b76879a281d0fe9
sunnyraj5555 <headheal2206@gmail.com>
2021-01-27 20:39:15 +0530

    Structure with variable and Functions

--- Struct_cpp_type.cpp
+++ Struct_cpp_type.cpp
@@ -9,14 +9,31 @@ struct book
 		char title[20];
 		float price;
 
+	public:				// Variables + Functions
+	void input()
+	{
+		cout << "Enter bookid, title and price of book:" <<endl;
+		cin >>bookid >>title >>price ;
+		if(bookid<0)
+		bookid =-bookid;
+	}
+
+	void display()
+	{
+		cout <<bookid <<" "<< title <<" "<<price <<endl;
+	}
 
 };
 
 
 int main()
 {
-
-
+	book b1;
+	b1.input();
+	b1.display();
+	//b1.bookid=-100;
+	//it cant be accessed now coz bookid is private
+	return 0;
 
 
 }
