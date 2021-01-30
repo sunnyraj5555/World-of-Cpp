b8926185afb84a2cbf4c06cb18031a727aec874e
Sunny Raj <59870615+sunnyraj5555@users.noreply.github.com>
2021-01-30 20:41:07 +0530

    Added addition of complex  Number

--- Complex_Class.cpp
+++ Complex_Class.cpp
@@ -15,6 +15,15 @@ class complex
   {
     cout << "a=" <<a <<"b="<<b;
   }
+	
+complex add(complex c)
+  {
+  	complex temp;
+  	temp.a=a+c.a;
+  	temp.b=b+c.b;
+  	return(temp);
+  }
+	
 };
 
 int main()
@@ -22,6 +31,14 @@ int main()
 	complex c1;
 	c1.set_data(4,5);
 	c1.show_data();
+	cout <<endl;
+	
+	c2.set_data(5,3);
+	c2.show_data();
+	cout <<endl;
+	
+        c3=c1.add(c2);
+	c3.show_data();
 
 	return 0;
 }
