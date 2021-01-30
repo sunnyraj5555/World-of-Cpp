3cf1e4f8bd7ba7f26d5e71a2463641ac44b28b57
sunnyraj5555 <headheal2206@gmail.com>
2021-01-30 17:07:02 +0530

    Watermelon Problem

---
+++ Competative_Programming/Watermelon.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int W;
+	cin >>W;
+	if(W==2)
+	{
+		cout <<"NO";
+	}
+	else if(W%2==0 && W<=100)
+	{
+		cout <<"YES";
+		
+		
+	}
+	else 
+	{
+		cout << "NO";
+	}
+
+return 0;	
+}
+
