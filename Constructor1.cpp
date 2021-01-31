5007ec2b9d5d626f8b5added90bf67721eaedeb4
sunnyraj5555 <headheal2206@gmail.com>
2021-01-31 14:18:42 +0530

    Constructor Created

--- Constructor1.cpp
+++ Constructor1.cpp
@@ -8,5 +8,12 @@ class Complex
 	private:
 		int a,b;
 	public:
-
+	Complex() //Same name as class with no return type
+	{ cout << "Hello Constructor!"<<endl;	}
 };
+int main()
+{
+	Complex c1,c2,c3; 
+	
+	return 0;
+}
